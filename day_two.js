948687258cbb0d725108edd4ab6b6678927fc3cb
Dana Sevcik <danasevcik@hotmail.com>
2019-12-02 11:44:36 -0500

    day two part 1 solution

---
+++ day_two.js
@@ -0,0 +1,20 @@
+function getFirst(arr) {
+  for (let i = 0; i < arr.length; i+=4) {
+    let num = arr[i];
+    if (num === 99) {
+      return arr[0];
+    } else if (num === 1) {
+      let next = arr[i + 1];
+      let nextNext = arr[i + 2];
+      let index = arr[i + 3];
+      arr[index] = arr[next] + arr[nextNext];
+    } else if (num === 2) {
+      let next = arr[i + 1];
+      let nextNext = arr[i + 2];
+      let index = arr[i + 3];
+      arr[index] = arr[next] * arr[nextNext];
+    }
+  }
+  return arr[0]
+}
+console.log(getFirst([1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,6,23,2,13,23,27,1,27,13,31,1,9,31,35,1,35,9,39,1,39,5,43,2,6,43,47,1,47,6,51,2,51,9,55,2,55,13,59,1,59,6,63,1,10,63,67,2,67,9,71,2,6,71,75,1,75,5,79,2,79,10,83,1,5,83,87,2,9,87,91,1,5,91,95,2,13,95,99,1,99,10,103,1,103,2,107,1,107,6,0,99,2,14,0,0]));
